50ebc160e4d158f38858cbb2628399187c355bce
byJonyB <by.jonyb@gmail.com>
2013-12-11 10:09:16 +0100

    Cambio de nombre de la web.

--- interfaces/cabecera.php
+++ interfaces/cabecera.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 	<head>
-		<title>Segunda mano</title>
+		<title>Cash4Trash</title>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 		<link href="css/ordenador.css" type="text/css" rel="stylesheet" />
 		<script type="text/javascript" src="js/sha512.js"></script>
@@ -12,7 +12,7 @@
 	<body>
 		<div id="container">
 			<div id="header"><!--div: menubar, adbar, sidebar-->
-				<h1 id="pr_title"><a href="index.php">Segunda mano</a></h1>
+				<h1 id="pr_title"><a href="index.php">Cash4Trash</a></h1>
 				<h1 id="conectado">&iexcl;Hola, <?php echo (isset($_SESSION['nombre']))?'<a href="panel.php">' . $_SESSION['nombre'] . '</a>' . '! <a href="conectar.php?desconectar">Desconectar.</a>':'an&oacute;nimo! <a href="conectar.php">Conectar.</a>' ?></h1>
 			</div>
 			<div class="clear"></div>
